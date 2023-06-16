b0f676ded89fa378c7264da97d38dd5e07221fa2
Mojalefa Tshetsi <smojalefa@ymail.com>
2023-06-15 21:34:26 -0700

    Hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main . A program that prints exactly a line
- * return: 0
+ * main - A program that prints exactly a line with put function
+ * return: 0 (success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
