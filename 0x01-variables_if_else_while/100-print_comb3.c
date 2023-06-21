3c065a6a5bbb37bb20609e6ba719188bb86fbe41
Mojalefa Tshetsi <smojalefa@ymail.com>
2023-06-21 09:37:00 -0700

    brains and materials

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,29 +9,24 @@
 
 int main(void)
 {
-
         int n, m;
-
-
-        for (n = '0'; n < '9'; n++)
-
+	
+	for (n = '0'; n < '9'; n++)
         {
-
-                for (m = n + 1; m <= '9'; m++)
+		for (m = n + 1; m <= '9'; m++)
                 {
-                        if (m != n)
-                        {
-                                putchar(n);
-				putchar(m);
-
-                                if (n == '8' && m == '9')
-				continue;
-                                {
-                                putchar(',');
-                                putchar(' ');
-                                }
-                        }
-                   }
+                if (m != n)
+                {
+                putchar(n);
+		putchar(m);
+		if (n == '8' && m == '9')
+		continue;
+                {
+                putchar(',');
+                putchar(' ');
+                }
+		}
+                }
 	}
         putchar('\n');
         return (0);
