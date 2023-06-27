512b750719791c8372d162fa719b454e4f0edca8
Mojalefa Tshetsi <smojalefa@ymail.com>
2023-06-27 02:52:36 -0700

    sacrifice the people to save the numbers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -29,7 +29,8 @@ int _atoi(char *s)
 			digit = s[i] - '0';
 			if (d % 2)
 				digit = -digit;
-			n = n * 10 + digit;f = 1;
+			n = n * 10 + digit;
+			f = 1;
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
 			f = 0;
@@ -37,7 +38,7 @@ int _atoi(char *s)
 		i++;
 	}
 	if (f == 0)
-		return (0);
-	
+	return (0);
+
 	return (n);
 }
