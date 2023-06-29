2262d80929c306112eb8eea8e3c58fbf8cd79bd4
Mojalefa Tshetsi <smojalefa@ymail.com>
2023-06-29 08:57:46 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,19 +3,19 @@
 
 /**
  * _strcmp - Compares two string character
- * @w1: the first character string to be compared
- * @w2: the second character string to be compared
+ * @s1: the first character string to be compared
+ * @s2: the second character string to be compared
  * Return: if str1 < str2, then a minus character
  * if str1 == str2, then gives 0.
  * if str1 > str2, then a plus character
  */
 
-int _strcmp(char *w1, char *w2);
+int _strcmp(char *s1, char *s2)
 {
-	while (*w1 && *w2 && *w1 == *w2)
+	while (*s1 && *s2 && *s1 == *s2)
 	{
-		w1++;
-		w2++;
+		s1++;
+		s2++;
 	}
-	return (*w1 - *w2);
+	return (*s1 - *s2);
 }
