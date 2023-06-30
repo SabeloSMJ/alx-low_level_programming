31263f1f0d65738f60d4a2db918378728a5eb2df
Mojalefa Tshetsi <smojalefa@ymail.com>
2023-06-29 17:46:30 -0700

    Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * string_toupper - takes lowercase characters to uppercase
+ * @str: The string character
+ * Return: A pointer to display changed string
+ */
+
+char *string_toupper(char *str)
+{
+	int index = 0;
+
+	while (str[index])
+	{
+		if (str[index] >= 'a' && str[index] <= 'z')
+		{
+			str[index] -= 32;
+			index++;
+		}
+	}
+	return (str);
+}
