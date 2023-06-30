22e370140c22cff4b04b43bd3caa28c9b7f1577b
Mojalefa Tshetsi <smojalefa@ymail.com>
2023-06-29 17:54:12 -0700

    Always look up

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -14,10 +14,8 @@ char *string_toupper(char *str)
 	while (str[index])
 	{
 		if (str[index] >= 'a' && str[index] <= 'z')
-		{
-			str[index] -= 32;
-			index++;
-		}
+		str[index] -= 32;
+		index++;
 	}
 	return (str);
 }
