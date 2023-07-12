eab489c10bd42f9cf25bbc78f4f51ab1296f0ecb
Mojalefa Tshetsi <smojalefa@ymail.com>
2023-07-11 18:11:03 -0700

    create_array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,14 +12,19 @@
 
 char *create_array(unsigned int size, char c)
 {
-	char *str;
 	unsigned int i;
+	char *s;
 
-	str = malloc(sizeof(char) * size);
-	if (size == 0 || str == NULL)
+	if (size == 0)
+		return (NULL);
+
+	s = malloc(size * sizeof(char));
+
+	if (s == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
-		str[i] = c;
-	return (str);
-}
+	{
+		s[i] = c;
+	}
+	return (s)
